89cc247557edae8d9fa0532689eb73fb0ac48943
Eduar Trujillo <eetgrisales@gmail.com>
2023-11-09 10:49:09 -0500

    Added validation so that an empty tweet cannot be added and an error message is displayed; Instead of selecting the textarea within the addTweet function, it was changed so that it is now available globally

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
 const form = document.querySelector('#form');
 const listTweets = document.querySelector('#list-tweets');
+const tweet = document.querySelector('#tweet');
 let tweets = [];
 
 eventListeners();
 
 function eventListeners() {
-    form.addEventListener('submit', addTweet);
+	form.addEventListener('submit', addTweet);
 }
 
 function addTweet(event) {
-    event.preventDefault();
+	event.preventDefault();
 
+	if (tweet.value.trim() === '') {
+		showError('You must write something');
+		return;
+	}
+}
+
+function showError(error) {
+	const errorMessage = document.createElement('P');
+	errorMessage.textContent = error;
+	errorMessage.classList.add('error');
+    tweet.insertAdjacentElement('afterend', errorMessage);
+    
+    setTimeout(() => {
+        errorMessage.remove();
+    }, 4000);
 }
